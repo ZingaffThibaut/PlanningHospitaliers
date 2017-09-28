7175c04e06fe81f44da19069ae06c2bf6a81cbcb
Salty57 <mrhugo57070@gmail.com>
2017-09-28 15:13:06 +0200

    correction

--- PHP/ConnexionMySQL.php
+++ PHP/ConnexionMySQL.php
@@ -1,5 +1,5 @@
 <?php
-$dns = 'mysql:infodb.iutmetz.univ-lorraine.fr;port=3306;dbname=wilcke3u_Planning';
+$dns = 'mysql:infodb.iutmetz.univ-lorraine.fr;port=3306;dbname=simonin45u_Planning';
 $utilisateur = 'simonin45u_appli';
 $motDePasse = '31513685';
 $objPdo = new PDO($dns,$utilisateur,$motDePasse);
--- PHP/Date.php
+++ PHP/Date.php
@@ -1,15 +1,16 @@
 <?php
 include"ConnexionMySQL.php";
 
-$date= new DateTime('2017-01-01');
-for($i=0;$i>37000;$i++){
-  $requete="INSERT INTO DATE VALUE('".$date->format('Y-m-d')."')";
+$date= new DateTime ('2017-01-01');
+while ($i < 37000) {
+  $requete="INSERT INTO Date VALUE('".$date->format('Y-m-d')."')";
   $result=$objPdo->prepare($requete);
   $result->execute();
   $err = $result->errorInfo();
   if(!empty($err[2])){
     echo $err[2];
   }
-  $date->modify('+ 1 day');
+  $date->modify('+1 day');
+  $i++;
 }
 ?>

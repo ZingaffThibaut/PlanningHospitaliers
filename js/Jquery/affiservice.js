ead06d73cf01cd4246e8526f5e1679a1ff28e962
maxime <maxime.cavelius@gmail.com>
2017-10-05 10:11:00 +0200

    maxime affiservice

---
+++ js/Jquery/affiservice.js
@@ -0,0 +1,28 @@
+$(function(){
+
+  $("#form_perso").submit(function(){
+    service = document.getElementById("option-service").value;
+    $.post(
+      "PHP/affiservice.php",
+      {
+        Id_service: service
+      },
+      function(data){
+          $("#tabservice").html(data);
+      }
+    );
+
+    return false;
+  });
+});
+
+$(document).ready(function(){
+  $.post(
+    "PHP/affiservice.php",
+    function(data){
+      if(data!=""){
+          $("#tabservice").html(data);
+      }
+    }
+  );
+});

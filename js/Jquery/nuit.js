34209877e097ec133340a554c7db7f8f69cad832
Thibaut Zingraff <thibaut.zing@gmail.com>
2017-10-05 13:34:54 +0200

    Correction mode nuit

--- js/Jquery/nuit.js
+++ js/Jquery/nuit.js
@@ -1,5 +1,5 @@
 $('#toggleNavColor').click(function() {
-  $('nav').toggleClass('navbag-light navbar-dark');
+  $('nav').toggleClass('navbar-light navbar-dark');
   $('nav').toggleClass('bg-light bg-dark');
-  $('body').toggleClass('bg-dg-light bg-dark');
+  $('body').toggleClass('bg-light bg-dark');
 });

6e5a30e72c8e237f2a9e0cab94347ac954ec4a72
Salty57 <mrhugo57070@gmail.com>
2017-10-08 15:42:07 +0200

    etat non fini

--- PHP/modifperso.php
+++ PHP/modifperso.php
@@ -4,7 +4,7 @@ session_start();
 if(isset($_POST)){
   include("connexion.php");
   extract($_POST);
-  $requete="UPDATE Personne SET Id_affi='1' WHERE Id_personne = '".$Id_personne."'";
+  $requete="UPDATE Personne SET Nom='".$Nom."', Prenom='".$Prenom."', Id_service='".$Service."' WHERE Id_personne = '".$Id_personne."'";
   $result=$bdd->prepare($requete);
   $result->execute();
   $err = $result->errorInfo();
@@ -12,7 +12,7 @@ if(isset($_POST)){
     echo $err[2];
     echo "Error";
   }else{
-    $requete="UPDATE Connexion SET Id_affi='1' WHERE Id_personne = '".$Id_personne."'";
+    $requete="UPDATE Connexion SET Id_lvl='".$Niveau."' WHERE Id_personne = '".$Id_personne."'";
     $result=$bdd->prepare($requete);
     $result->execute();
     $err = $result->errorInfo();
@@ -20,7 +20,7 @@ if(isset($_POST)){
       echo $err[2];
       echo "Error";
     }else{
-      echo "ok";
+      echo 'ok';
     }
   }
 }

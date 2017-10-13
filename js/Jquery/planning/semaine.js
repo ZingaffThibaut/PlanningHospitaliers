e3fd3985b275d25e006291463333677c84987b90
Salty57 <mrhugo57070@gmail.com>
2017-10-13 11:33:28 +0200

    Affichage du planning OK

--- js/Jquery/planning/semaine.js
+++ js/Jquery/planning/semaine.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
   var service = $(location).attr('search');
   service     = service.substring(9);
-  $("#nomService").html(' ' + service);
-  var date = document.getElementById("Date").innerHTML;
+  $("#nomService").html(service);
+
+
+  var date = new Date();
+  var affi = "";
+  var el = affi.concat(date.getFullYear(),"-",date.getMonth()+1,"-",date.getDate());
 
   $.post(
     "PHP/planning/Semaine.php",
     {
       service : service,
-      date : date
+      date : el
     },
     function(data){
       $("#tab").html(data);
@@ -36,6 +40,47 @@ $(document).ready(function(){
     }
   );
 });
+
+
+function Planningchar(){
+  service = document.getElementById("nomService").innerHTML;
+  var date = new Date(document.getElementById("Date").value);
+  var affi = "";
+  var el = affi.concat(date.getFullYear(),"-",date.getMonth()+1,"-",date.getDate());
+  $.post(
+    "PHP/planning/Semaine.php",
+    {
+      service : service,
+      date : el
+    },
+    function(data){
+      $("#tab").html(data);
+      var array = document.getElementById("tableau").rows;
+      var longeur = array.length;
+      var i=1;
+      while(i<longeur){
+        for(y=0;y<=7;y++){
+          if(array[i].cells.item(y)){
+            switch (array[i].cells.item(y).innerHTML){
+              case "T":
+              array[i].cells.item(y).className="bg-success text-white";
+              break;
+              case "RC":
+              array[i].cells.item(y).className="bg-warning text-white";
+              break;
+              case "RH":
+              array[i].cells.item(y).className="bg-danger text-white";
+              break;
+            }
+          }
+        }
+        i++;
+      }
+    }
+  );
+}
+
+
 
 
 function daynow(){

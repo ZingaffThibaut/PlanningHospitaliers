36171d2aaae06dabca76e6953b89aaa9685ad8c3
test@example.fr <test@exemple.fr>
2017-10-25 16:29:12 +0200

    Import csv

--- PHP/importcsv.php
+++ PHP/importcsv.php
@@ -1,47 +1,85 @@
 <?php
-  if(!empty($_FILES['fichierCSV']['name'])){
 
-    $extensionAutorise  = array("csv");
-    $extension          = end(explode(".", $_FILES['fichierCSV']['name']));
+include("connexion.php");
 
-    $ligne = 1;
+if(!empty($_FILES['fichierCSV']['name'])){
 
-    if(in_array($extension, $extensionAutorise)){
+  $extensionAutorise  = array("csv");
+  $extension          = end(explode(".", $_FILES['fichierCSV']['name']));
 
-      $file_data  = fopen($_FILES['fichierCSV']['tmp_name'],"r");
-      while($row = fgetcsv($file_data,1000,';')){
-        $champs = count($row);
+  $ligne = 1;
+  if(in_array($extension, $extensionAutorise)){
 
-        if($ligne >1){
-          $Nom = $row[0];
-          $Prenom = $row[1];
-          $Date = $row[2];
-          $Periode = $row[3];
-          $Dispo = $row[4];
+    $file_data  = fopen($_FILES['fichierCSV']['tmp_name'],"r");
+    $up = 0;
+    $in = 0;
+    $er = 0;
+    while($row = fgetcsv($file_data,1000,';')){
+      $champs = count($row);
 
-          $requete = "Select id_personne From Personne Where Nom = ".$Nom." AND Prenom = ".$Prenom;
+      if($ligne >1){
+        $Nom = $row[0];
+        $Prenom = $row[1];
+        $date2 =  new DateTime(substr($row[2],6,4)."-".substr($row[2],3,2)."-".substr($row[2],0,2));
+        $Id_periode = $row[3];
+        $Dispo = $row[4];
+        $date = $date2->format('Y-m-d');
+
+        $requete = "Select Id_dispo From Disponibilite Where Nom_dispo = '".$Dispo."' ";
+        $result=$bdd->prepare($requete);
+        $result->execute();
+        $row2 = $result ->fetch();
+        $Choix = $row2['Id_dispo'];
+        $requete = "Select Id_personne, Id_service From Personne Where Nom = '".$Nom."' AND Prenom = '".$Prenom."'";
+        $result=$bdd->prepare($requete);
+        $result->execute();
+        $nb = $result->rowCount();
+        if($nb != 0){
+          $row2 = $result ->fetch();
+          $Id_personne = $row2['Id_personne'];
+          $Id_service = $row2['Id_service'];
+
+          $requete="SELECT * FROM Planning WHERE Id_personne='".$Id_personne."' AND Date ='".$date."' AND Id_periode ='".$Id_periode."' ";
           $result=$bdd->prepare($requete);
           $result->execute();
-
-          while($row = $result ->fetch()){
-            $Id_personne = $row['id_personne'];
+          $err = $result->errorInfo();
+          if(!empty($err[2])){
+            $er++;
+          }else{
+            $nb = $result->rowCount();
+            if($nb!=0){
+              $requete="UPDATE Planning SET Id_dispo ='".$Choix."' WHERE Id_personne='".$Id_personne."' AND Date ='".$date."' AND Id_periode ='".$Id_periode."' ";
+              $result=$bdd->prepare($requete);
+              $result->execute();
+              $err = $result->errorInfo();
+              if(!empty($err[2])){
+                $er++;
+              }
+              $up++;
+            }else{
+              $requete="INSERT INTO Planning VALUE ('".$Id_personne."','".$Id_service."','".$date."','".$Id_periode."','".$Choix."')";
+              $result=$bdd->prepare($requete);
+              $result->execute();
+              $err = $result->errorInfo();
+              if(!empty($err[2])){
+                $er++;
+              }
+              $in++;
+            }
           }
-          $autrementonsenfou = new Date($Date);
-          $variableermerde2 = $Date->format('Y-m-d');
-
-          $requete = "Select Id_dispo From Disponibilite Where Nom_periode =".$Dispo;
-          $result=$bdd->prepare($requete);
-          $result->execute();
+        }else{
+          echo("error 2");
         }
-
-        $ligne++;
       }
-    }else{
-      echo("error 2");
+      $ligne++;
     }
+    echo"Importation réussi <br>".$in." ligne insert et ".$up." ligne modifié ".$er."error";
   }else{
-    echo("error 1");
+    echo("Fichier de mauvais type, utiliser le ficher CSV fourni");
   }
+}else{
+  echo("Merci de selectionner un ficher");
+}
 
 
 ?>
--- js/Jquery/importcsv.js
+++ js/Jquery/importcsv.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $("#formUpload").submit(function(e){
     e.preventDefault();
     $.ajax({

6869cffb3d4102ad24a08f1067fe11fcc373c604
Thibaut Zingraff <thibaut.zing@gmail.com>
2017-10-25 14:39:40 +0200

    maj

--- PHP/importcsv.php
+++ PHP/importcsv.php
@@ -1,5 +1,4 @@
 <?php
-  print_r($_POST);
   if(!empty($_FILES['fichierCSV']['name'])){
 
     $extensionAutorise  = array("csv");
@@ -7,19 +6,35 @@
 
     $ligne = 1;
 
-
     if(in_array($extension, $extensionAutorise)){
 
       $file_data  = fopen($_FILES['fichierCSV']['tmp_name'],"r");
       while($row = fgetcsv($file_data,1000,';')){
-        $champs = count($row)
+        $champs = count($row);
 
-        if($ligne > 1){
-          echo "Personne : ".$row[0].'<br>';
-          for($i=1; $i<$champs; $i ++){
-            echo '|'.$row[$i].'|';
+        if($ligne >1){
+          $Nom = $row[0];
+          $Prenom = $row[1];
+          $Date = $row[2];
+          $Periode = $row[3];
+          $Dispo = $row[4];
+
+          $requete = "Select id_personne From Personne Where Nom = ".$Nom." AND Prenom = ".$Prenom;
+          $result=$bdd->prepare($requete);
+          $result->execute();
+
+          while($row = $result ->fetch()){
+            $Id_personne = $row['id_personne'];
           }
+          $autrementonsenfou = new Date($Date);
+          $variableermerde2 = $Date->format('Y-m-d');
+
+          $requete = "Select Id_dispo From Disponibilite Where Nom_periode =".$Dispo;
+          $result=$bdd->prepare($requete);
+          $result->execute();
         }
+
+        $ligne++;
       }
     }else{
       echo("error 2");
